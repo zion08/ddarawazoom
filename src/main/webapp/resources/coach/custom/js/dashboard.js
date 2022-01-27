1fb1d011e1eba60ce8a482004501c18e3dca4369
Seung-Hwan <93048181+JeonSeungHwaan@users.noreply.github.com>
2022-01-27 11:03:45 +0900

    월별 매출 그래프 일부 수정

--- src/main/webapp/resources/coach/custom/js/dashboard.js
+++ src/main/webapp/resources/coach/custom/js/dashboard.js
@@ -1,69 +1,59 @@
-/* globals Chart:false, feather:false */
+	/* globals Chart:false, feather:false */
 
-$(document).ready(function(){
-  'use strict'
-  
-  var amountData = new Array();
-  var paidDate = new Array();
-  
-  $.ajax({
-	url : "/coachroom/getPayment",
-	type : "post",
-	contentType : "application/json; charset=UTF-8",
-	success : function(data){
-		console.log(data);
-		
-		for (var i = 0; i < data.length; i++){
-			
+	$(document).ready(function(){
+	  'use strict'
+	  
+	  var amountData = new Array();
+	  var payData = new Array();
+	  
+	  $.ajax({
+		url : "/coachroom/getPayment",
+		type : "post",
+		contentType : "application/json; charset=UTF-8",
+		success : function(data){
+			console.log(data);
+	
+			for(var key in data){
+				console.log(key);
+				payData.push(key);
+				
+				console.log(data[key]);
+				amountData.push(data[key]);
+			}
+			console.log(payData);
+			console.log(amountData);
 		}
-	}
-  });
-  
-  feather.replace({ 'aria-hidden': 'true' })
-
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-})
+	  });
+	  
+	  feather.replace({ 'aria-hidden': 'true' })
+	
+	  // Graphs
+	  var ctx = document.getElementById('myChart')
+	  // eslint-disable-next-line no-unused-vars
+	  var myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	      labels: payData,
+	      datasets: [{
+	        data: amountData,
+	        lineTension: 0,
+	        backgroundColor: 'transparent',
+	        borderColor: '#007bff',
+	        borderWidth: 4,
+	        pointBackgroundColor: '#007bff'
+	      }]
+	    },
+	    options: {
+	      scales: {
+	        yAxes: [{
+	          ticks: {
+	            beginAtZero: false
+	          }
+	        }]
+	      },
+	      legend: {
+	        display: false
+	      }
+	    }
+	  })
+	})
