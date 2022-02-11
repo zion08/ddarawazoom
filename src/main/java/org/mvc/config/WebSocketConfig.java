ab5aed1f80060034b1c64c46261744c7c81e5b60
unknown <damhee6624@naver.com>
2022-02-11 20:53:24 +0900

    오바라이드 추가

--- src/main/java/org/mvc/config/WebSocketConfig.java
+++ src/main/java/org/mvc/config/WebSocketConfig.java
@@ -16,6 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer{
 	@Autowired
 	SocketHandler socketHandler;
 	
+	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(socketHandler, "/chating/{roomNumber}");
 	}
