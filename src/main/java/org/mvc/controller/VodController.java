69bf5a8c76c7dc69c4f778eed7f6a6f6837e9cbc
zion <syoul@kakao.com>
2022-02-15 03:28:41 +0900

    sy-테스트 코드 삭제
    sy-테스트 코드 삭제

--- src/main/java/org/mvc/controller/VodController.java
+++ src/main/java/org/mvc/controller/VodController.java
@@ -1,20 +1,12 @@
 package org.mvc.controller;
 
-import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.json.simple.parser.ParseException;
-import org.mvc.Component.Crawling;
-import org.mvc.bean.PaymentDTO;
-import org.mvc.bean.YoutubeDTO;
 import org.mvc.service.YoutubeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
  
